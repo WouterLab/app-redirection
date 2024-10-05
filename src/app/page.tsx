2063cbef93cbd4d27971e327466f2d4430657ceb
WouterLab <panov135@mail.ru>
2024-10-05 13:11:23 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 function Home() {
   const router = useRouter();
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    router.replace('http://192.168.0.15:3000');
-  }, [router]);
+    setLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    if (loaded) {
+      router.push('http://192.168.0.15:3000');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [loaded]);
 
   return null;
 }
